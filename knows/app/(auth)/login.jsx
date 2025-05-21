8e31a226bca1e5be41794a63a44603e786826138
tchasinga <tchasingajacques@gmail.com>
2025-05-21 18:00:35 +0300

    fix: Refactor login component to improve input handling and add loading state

--- knows/app/(auth)/login.jsx
+++ knows/app/(auth)/login.jsx
@@ -1,15 +1,21 @@
-import { View, Text, TextInput } from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator
+} from 'react-native'
 import React from 'react'
 import styles from '../../assets/styles/login.styles'
 import { useState } from 'react'
 import { Image } from 'expo-image'
-import Ionicons from '../../node_modules/@expo/vector-icons/Ionicons.d';
+import Ionicons from '../../node_modules/@expo/vector-icons/Ionicons.d'
 
 export default function Login () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [showPassword, setShowPassword] = useState(false)
-  const [loading, setLoading] = useState(false)
+  const [isLoading, setLoading] = useState(false)
 
   const handleLogin = () => {}
 
@@ -23,31 +29,41 @@ export default function Login () {
       </View>
       {/* addinng form here for data collection */}
       <View style={styles.card}>
-       <View style={styles.formContainer}>
-        {/* EMAIL */}
-        <View style={styles.inputGroup}>
-          <Text style={styles.label}>Email</Text>
-          <View style={styles.inputContainer}>
-            <Ionicons name="mail-outline" size={24} color="black" style={styles.inputIcon} />
-            <TextInput
-              style={styles.input}
-              placeholder="Enter your email"
-              value={email}
-              onChangeText={setEmail}
-              keyboardType="email-address"
-              autoCapitalize="none"
-            />
+        <View style={styles.formContainer}>
+          {/* EMAIL */}
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Email</Text>
+            <View style={styles.inputContainer}>
+              <Ionicons
+                name='mail-outline'
+                size={24}
+                color='black'
+                style={styles.inputIcon}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder='Enter your email'
+                value={email}
+                onChangeText={setEmail}
+                keyboardType='email-address'
+                autoCapitalize='none'
+              />
+            </View>
           </View>
         </View>
-       </View>
 
         <View style={styles.inputGroup}>
           <Text style={styles.label}>Password</Text>
           <View style={styles.inputContainer}>
-            <Ionicons name="lock-closed-outline" size={24} color="black" style={styles.inputIcon} />
+            <Ionicons
+              name='lock-closed-outline'
+              size={24}
+              color='black'
+              style={styles.inputIcon}
+            />
             <TextInput
               style={styles.input}
-              placeholder="Enter your password"
+              placeholder='Enter your password'
               value={password}
               onChangeText={setPassword}
               secureTextEntry={!showPassword}
@@ -55,11 +71,22 @@ export default function Login () {
             <Ionicons
               name={showPassword ? 'eye-off-outline' : 'eye-outline'}
               size={24}
-              color="black"
+              color='black'
               onPress={() => setShowPassword(!showPassword)}
             />
           </View>
         </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleLogin}
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <ActivityIndicator size='small' color='#fff' />
+          ) : (
+            <Text style={styles.buttonText}>Login</Text>
+          )}
+        </TouchableOpacity>
       </View>
     </View>
   )
