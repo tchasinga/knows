e5cc4075b432ad60dc33833a2880885a887229c0
tchasinga <tchasingajacques@gmail.com>
2025-05-21 14:07:25 +0300

    fix: Correct middleware import path for route protection

--- backend/routes/book.routes.js
+++ backend/routes/book.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {createBook } from '../controllers/book.controllers.js'
-import { protect } from '../midlleware/protectrouter.js'
+import { protect } from '../middleware/protectrouter.js'
 
 const router = express.Router()
 
