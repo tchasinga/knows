e029cc5971310799fb5fe683e84c845499510846
tchasinga <tchasingajacques@gmail.com>
2025-05-21 16:13:23 +0300

    feat: Wrap RootLayout in SafeAreaProvider for improved layout handling

--- knows/app/_layout.jsx
+++ knows/app/_layout.jsx
@@ -1,10 +1,13 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-export default function RootLayout() {
+export default function RootLayout () {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="(auth)" options={{ title: "Authentication" }} />
-    </Stack>
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='index' options={{ title: 'Home' }} />
+        <Stack.Screen name='(auth)' options={{ title: 'Authentication' }} />
+      </Stack>
+    </SafeAreaProvider>
   )
 }
