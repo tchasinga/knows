579685f37b8c76b628b61bdffcc9e0a3b0219489
tchasinga <tchasingajacques@gmail.com>
2025-05-21 15:23:17 +0300

    fix: Clean up formatting and improve readability in Index component

--- knows/app/index.jsx
+++ knows/app/index.jsx
@@ -1,19 +1,23 @@
-import { Text, View , StyleSheet} from "react-native";
+import { Text, View, StyleSheet } from 'react-native'
+import { Image } from 'expo-image'
 
-export default function Index() {
+export default function Index () {
   return (
     <View style={styles.Container}>
       <Text>To edit this screen... now</Text>
+      <Image
+        source={{ uri: 'https://storyset.com/illustration/bookshop/rafiki' }}
+        style={{ width: 150, height: 150 }}
+      />
     </View>
-  );
+  )
 }
-
 
 // adding a style
 const styles = StyleSheet.create({
   Container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
