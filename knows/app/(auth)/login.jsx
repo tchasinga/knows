e08348e2e5a6f71a2ca3261b7ca28f4ab876806a
tchasinga <tchasingajacques@gmail.com>
2025-05-21 17:24:12 +0300

    fix: Initialize state variables in login component for form handling

--- knows/app/(auth)/login.jsx
+++ knows/app/(auth)/login.jsx
@@ -1,7 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import styles from '../../assets/styles/login.styles'
+import { useState } from 'react'
 
 export default function login() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+
   return (
     <View>
       <Text>login</Text>
