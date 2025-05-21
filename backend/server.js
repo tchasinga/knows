36439ca1153d9158e1d0782f6a8c400a6e0d086e
tchasinga <tchasingajacques@gmail.com>
2025-05-21 10:45:15 +0300

    Fix main entry point in package.json and enhance server.js for MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
 import express from 'express';
-
-
+import dotenv from "dotenv";
+import linksToMogoDbUrl from './db/linksToMogoDbUrl';
 
 // innitialize express app
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+// connect to MongoDB
+app.listen(PORT, () => {
+   try {
+       linksToMogoDbUrl();
+       console.log(`✅ Server is running on port ${PORT}`);
+   } catch (error) {
+       console.error("❌ Failed to start server:", error.message);
+   }
+}
+);
