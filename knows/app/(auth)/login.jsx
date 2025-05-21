cdd8ceedb54c0c8ccffb10fab94830ced967a132
tchasinga <tchasingajacques@gmail.com>
2025-05-21 17:39:26 +0300

    fix: Correct import path for styles and update function name to PascalCase

--- knows/app/(auth)/login.jsx
+++ knows/app/(auth)/login.jsx
@@ -1,21 +1,29 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import styles from '../../assets/styles/login.styles.js'
+import styles from '../../assets/styles/login.styles'
 import { useState } from 'react'
+import { Image } from 'expo-image'
 
-export default function login() {
-
+export default function Login () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [showPassword, setShowPassword] = useState(false)
   const [loading, setLoading] = useState(false)
 
-const handleLogin = () => {
-}
+  const handleLogin = () => {}
 
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <View style={styles.topIllustration}>
+        <Image
+          source={require('../../assets/images/Bookshop-amico.png')}
+          style={styles.illustrationImage}
+        />
+      </View>
+      {/* addinng form here for data collection */}
+      <View style={styles.card}>
+       <View style={styles.formContainer}></View>
+      </View>
     </View>
   )
 }
