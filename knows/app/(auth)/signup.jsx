6ea407fd6fa6243a89ca9ee55ddd1adb6e758203
tchasinga <tchasingajacques@gmail.com>
2025-05-22 09:26:59 +0300

    feat: Implement signup component with keyboard avoidance for improved user experience

--- knows/app/(auth)/signup.jsx
+++ knows/app/(auth)/signup.jsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, KeyboardAvoidingView, Platform } from 'react-native'
 import React from 'react'
+import styles from '../../assets/styles/signup.styles'
 
 export default function signup() {
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
+   <KeyboardAvoidingView
+         style={{ flex: 1 }}
+         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+       >
+     <View style={styles.container}>
+       <View style={styles.card}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Create an Account</Text>
+          <Text style={styles.subtitle}>Sign up to get started</Text>
+        </View>
+       </View>
+     </View>
+    </KeyboardAvoidingView>
   )
 }
