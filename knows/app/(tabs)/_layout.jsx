853218eb57b5e4b08eeda3e0d84fa8a10437e8e1
tchasinga <tchasingajacques@gmail.com>
2025-05-23 11:07:33 +0300

    fix: Improve formatting and consistency in TabsLayout component

--- knows/app/(tabs)/_layout.jsx
+++ knows/app/(tabs)/_layout.jsx
@@ -5,25 +5,33 @@ import Ionicons from '@expo/vector-icons/Ionicons.js'
 export default function TabsLayout () {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name='index' 
-      options={{
-         title: 'Home',
-         tabBarIcon: ({ color, size }) => (
-           <Ionicons name='home-outline' size={size} color={color} />
-         ),
-       }} />
-      <Tabs.Screen name='create' options={{
-         title: 'Create',
-         tabBarIcon: ({ color, size }) => (
-           <Ionicons name='add-circle-outline' size={size} color={color} />
-         ),
-       }} />
-      <Tabs.Screen name='profile' options={{
-         title: 'Profile',
-         tabBarIcon: ({ color, size }) => (
-           <Ionicons name='person-outline' size={size} color={color} />
-         ),
-       }} />
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='home-outline' size={size} color={color} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='create'
+        options={{
+          title: 'Create',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='add-circle-outline' size={size} color={color} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='person-outline' size={size} color={color} />
+          )
+        }}
+      />
     </Tabs>
   )
 }
