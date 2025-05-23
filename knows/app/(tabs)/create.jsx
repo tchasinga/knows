ab30d4eeed2355f0c79a862d87418a9d751ddf35
tchasinga <tchasingajacques@gmail.com>
2025-05-23 15:50:03 +0300

    fix: Add state variables for caption, rating, and image in Create component

--- knows/app/(tabs)/create.jsx
+++ knows/app/(tabs)/create.jsx
@@ -4,6 +4,9 @@ import { View, Text } from 'react-native'
 export default function Create() {
 
   const [title, setTitle] = useState('') 
+  const [caption, setCaption] = useState('')
+  const [ratinng, setRating] = useState(2)
+  const [image, setImage] = useState(null)
 
   return (
     <View>
