50d77b555ac8c95cfd40f202845a7ca4a781aadb
tchasinga <tchasingajacques@gmail.com>
2025-05-23 10:41:54 +0300

    fix: Update Home screen name in layout and clean up imports

--- knows/app/_layout.jsx
+++ knows/app/_layout.jsx
@@ -1,14 +1,17 @@
-import { Stack } from 'expo-router'
+import { Stack, useRouter, useSegments } from 'expo-router'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import SafeScreen from '../components/SafeScreen'
 import { StatusBar } from 'expo-status-bar'
 
 export default function RootLayout () {
+
+  const router = useRouter()
+  const segments = useSegments()
+
   return (
     <SafeAreaProvider>
       <SafeScreen>
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name='index' options={{ title: 'Home' }} />
           <Stack.Screen name='(auth)' options={{ title: 'Authentication' }} />
           <Stack.Screen name='(tabs)' options={{ title: 'Tabs' }} />
         </Stack>
