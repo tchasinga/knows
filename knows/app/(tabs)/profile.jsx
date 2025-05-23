bd0671b32ddd570bf1cd9527936505e6b75ddd52
tchasinga <tchasingajacques@gmail.com>
2025-05-23 15:34:16 +0300

    fix: Refactor Profile component to include user information and logout functionality

--- knows/app/(tabs)/profile.jsx
+++ knows/app/(tabs)/profile.jsx
@@ -1,10 +1,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import useAuthStore from '../../store/authStore'
 
-export default function profile() {
+export default function Profile() {
+  const { user,logout } = useAuthStore()
+
+
+  const handleLogout = () => {
+    logout()
+  }
+
+
   return (
     <View>
       <Text>profile</Text>
+      <Text>{user?.name}</Text>
+      <Text>{user?.email}</Text>
+      <Text onPress={handleLogout}>Logout</Text>
     </View>
   )
 }
