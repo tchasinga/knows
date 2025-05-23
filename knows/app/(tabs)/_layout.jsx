d8016dc5a852be1d8081c28b3968ab2b23343e70
tchasinga <tchasingajacques@gmail.com>
2025-05-23 10:58:28 +0300

    fix: Add screenOptions to Tabs component in TabsLayout

--- knows/app/(tabs)/_layout.jsx
+++ knows/app/(tabs)/_layout.jsx
@@ -3,7 +3,7 @@ import { Tabs } from 'expo-router'
 // add tabs layout
 export default function TabsLayout () {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name='index' options={{ title: 'Home' }} />
       <Tabs.Screen name='create' options={{ title: 'Create' }} />
       <Tabs.Screen name='profile' options={{ title: 'Profile' }} />
