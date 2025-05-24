b768411ca3da61712726b73eecf057ddee6b3626
tchasinga <tchasingajacques@gmail.com>
2025-05-24 11:25:16 +0300

    fix: Implement renderItem function to display book details in FlatList

--- knows/app/(tabs)/index.jsx
+++ knows/app/(tabs)/index.jsx
@@ -2,6 +2,7 @@ import { View, Text, Alert, FlatList } from 'react-native'
 import { useEffect, useState } from 'react'
 import useAuthStore from '../../store/authStore.js'
 import styles from '../../assets/styles/home.styles.js'
+import Image from "expo-image"
 
 export default function Index () {
   const { token } = useAuthStore()
@@ -17,7 +18,22 @@ export default function Index () {
 
   const handleLoadMore = async () => {}
 
-  const renderItem = ({ item }) => {}
+  const renderItem = ({ item }) => (
+    <View style={styles.bookCard}>
+      
+      <View style={styles.bookHeader}>
+        <View style={styles.userInfo}>
+          <Image
+            source={{ uri: item.user.profilepic }}
+            style={styles.avatar}
+            contentFit="cover"
+          />
+          <Text style={styles.username}>{item.user.name}</Text>
+        </View>
+      </View>
+
+    </View>
+  )
 
   useEffect(() => {
     fetchBooks()
