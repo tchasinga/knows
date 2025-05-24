3f6d59fa1dc0e7b17c780aaa5170389824c52d9d
tchasinga <tchasingajacques@gmail.com>
2025-05-24 10:15:37 +0300

    fix: Improve navigation readiness handling in RootLayout component

--- knows/app/_layout.jsx
+++ knows/app/_layout.jsx
@@ -1,30 +1,36 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Stack, useRouter, useSegments } from 'expo-router'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import SafeScreen from '../components/SafeScreen'
 import { StatusBar } from 'expo-status-bar'
 import useAuthStore from '../store/authStore.js'
 
-
-
-export default function RootLayout () {
+export default function RootLayout() {
   const router = useRouter()
   const segments = useSegments()
-  const {checkAuth ,user, token } = useAuthStore()
+  const { checkAuth, user, token } = useAuthStore()
 
-  // Check if user is logged in
+  const [isNavigationReady, setIsNavigationReady] = useState(false)
+
+  // Run auth check
   useEffect(() => {
     checkAuth()
   }, [checkAuth])
 
-  // Redirect to login if not logged in
+  // Run once after first mount
   useEffect(() => {
+    setIsNavigationReady(true)
+  }, [])
+
+  useEffect(() => {
+    if (!isNavigationReady) return
+
     if (!user && !token && segments[0] !== '(auth)') {
       router.replace('(auth)')
     } else if (user && token && segments[0] === '(auth)') {
       router.replace('(tabs)')
     }
-  }, [user, token, router, segments])
+  }, [user, token, router, segments, isNavigationReady])
 
   return (
     <SafeAreaProvider>
