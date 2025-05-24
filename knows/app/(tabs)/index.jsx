1540f07a9411b4085e8a7d9af8c5bd99864cfe25
tchasinga <tchasingajacques@gmail.com>
2025-05-24 10:22:32 +0300

    fix: Add state management and fetchBooks function in index component

--- knows/app/(tabs)/index.jsx
+++ knows/app/(tabs)/index.jsx
@@ -1,9 +1,21 @@
 import { View, Text } from 'react-native'
+import { useEffect, useState } from 'react'
 import useAuthStore from '../../store/authStore.js'
 
 export default function Index () {
 
   const { token } = useAuthStore()
+  const [books, setBooks] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [refreshing, setRefreshing] = useState(false)
+  const [page, setPage] = useState(1)
+  const [hasMore, setHasMore] = useState(true)
+
+  const fetchBooks = async () => {}
+
+  useEffect(() =>{
+    fetchBooks()
+  })
 
   return (
     <View>
