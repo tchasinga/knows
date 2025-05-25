cc9a401d55366731d70dd058e4b8f0ab34555d25
tchasinga <tchasingajacques@gmail.com>
2025-05-25 20:55:41 +0300

    fix: Update password strength requirement to a minimum of 8 characters

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -29,10 +29,10 @@ export const signup = async (req, res) => {
     }
 
     // Validate password strength
-    if (password.length < 6) {
+    if (password.length < 8) {
       return res.status(400).json({
         success: false,
-        message: 'Password must be at least 6 characters long'
+        message: 'Password must be at least 8 characters long'
       })
     }
 
